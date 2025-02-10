01fa257d8ac994bd6193a0d0b6c925f9d209a10a
Ishan Aapan <ishanaapan02@gmail.com>
2025-02-10 18:58:22 +0530

    about us changes

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -3,21 +3,18 @@ import Home from "../pages/Home";
 import Doctors from "../pages/Doctors";
 import Appointments from "../pages/Appointments";
 import ApplyForDoctor from "../pages/ApplyForDoctor";
-import AboutUS from "../pages/AboutUS";
-import ContactUS from "../pages/ContactUS";
 import Profile from "../pages/Profile";
+import NavBar from "../components/NavBar";
 const AppRoutes = () => {
   return (
     <>
       <BrowserRouter>
+        <NavBar />
         <Routes>
-          {/* <Route index element={<NavBar/>}></Route> */}
           <Route index element={<Home />} />
           <Route path="doctors" element={<Doctors />} />
           <Route path="appointments" element={<Appointments />} />
           <Route path="applyfordoctor" element={<ApplyForDoctor />} />
-          <Route path="aboutus" element={<AboutUS />} />
-          <Route path="contactus" element={<ContactUS />} />
           <Route path="profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
